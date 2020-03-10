b6955cba769005ed1f30df20ba2cf5ad8411b690
João Vitor Viegas Rêgo <jvrviegas@gmail.com>
2020-03-09 23:49:31 -0300

    D - Routes - Add Deliverymans store route

--- src/routes.js
+++ src/routes.js
@@ -6,19 +6,26 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import FileController from './app/controllers/FileController';
+import DeliverymanController from './app/controllers/DeliverymanController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
+// Users
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
+// Authenticated Users
 routes.put('/users', UserController.update);
+
 routes.post('/recipients', RecipientController.store);
+
+// Deliveryman Management
+routes.post('/deliverymans', DeliverymanController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
