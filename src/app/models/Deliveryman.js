74a4f8ffdcacc8a3602a6e17442bfad6f3932373
João Vitor Viegas Rêgo <jvrviegas@gmail.com>
2020-03-09 23:12:57 -0300

    D - Models - Create Deliveryman model

---
+++ src/app/models/Deliveryman.js
@@ -0,0 +1,25 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Deliveryman extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        id: Sequelize.INTEGER,
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        avatar_id: Sequelize.INTEGER,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
+  }
+}
+
+export default Deliveryman;
