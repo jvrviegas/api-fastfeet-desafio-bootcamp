7593855be96300b3383fb4531a40961aac245fd0
João Vitor Viegas Rêgo <jvrviegas@gmail.com>
2020-03-09 23:49:56 -0300

    D - Deliveryman Controller - Add store method

--- src/app/controllers/DeliverymanController.js
+++ src/app/controllers/DeliverymanController.js
@@ -1,5 +1,37 @@
+import * as Yup from 'yup';
 import Deliveryman from '../models/Deliveryman';
+import User from '../models/User';
 
-class DeliverymanController {}
+class DeliverymanController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const userCheckAdmin = await User.findOne({
+      where: { id: req.userId, admin: true },
+    });
+
+    if (!userCheckAdmin) {
+      return res
+        .status(401)
+        .json({ error: 'Only administrators can register deliverymans.' });
+    }
+
+    const { name, email } = await Deliveryman.create(req.body);
+
+    return res.json({
+      name,
+      email,
+    });
+  }
+}
 
 export default new DeliverymanController();
