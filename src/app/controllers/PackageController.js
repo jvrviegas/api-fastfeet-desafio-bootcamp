b9d410b21128cb7700299bebe2fa109571608ba4
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-03-13 12:33:58 -0300

    D - Package Controller - Add store method

--- src/app/controllers/PackageController.js
+++ src/app/controllers/PackageController.js
@@ -1,3 +1,29 @@
-class PackageController {}
+import * as Yup from 'yup';
+import Package from '../models/Package';
+
+class PackageController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      recipient_id: Yup.number().required(),
+      deliveryman_id: Yup.number().required(),
+      signature_id: Yup.number(),
+      product: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const pack = await Package.create(req.body);
+
+    if (!pack) {
+      return res.status(500).json({
+        error: 'Fail to create the package, try again in a few seconds',
+      });
+    }
+
+    return res.status(201).json(pack);
+  }
+}
 
 export default new PackageController();
