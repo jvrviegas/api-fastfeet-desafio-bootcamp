70bbdae3b883e850cbb1f7ea3589c9de7db3cf2d
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-04-03 09:10:44 -0300

    M - User Controller - Add avatar to user update return

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -68,14 +69,23 @@ class UserController {
       return res.status(401).json({ error: 'Password does not match!' });
     }
 
-    const { id, name, admin, avatar_id } = await user.update(req.body);
+    await user.update(req.body);
+
+    const { id, name, avatar } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     return res.json({
       id,
       name,
       email,
-      admin,
-      avatar_id,
+      avatar,
     });
   }
 }
